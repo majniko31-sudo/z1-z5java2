f711cbc2f5636015b034e707865a7c06e6cf45d5
majniko31-sudo <majniko31@gmail.com>
2025-09-21 17:50:31 +0200

    majer.js

--- majer.js
+++ majer.js
@@ -1,2 +1,5 @@
+document.write("Mój numer w dzienniku to 11<br><br>");
+document.write("Pierwszy wiersz to:&nbsp;&nbsp;6&nbsp;&nbsp;Nie&nbsp;&nbsp;Tak&nbsp;&nbsp;6<br>");
+document.write("Drugi wiersz to:&nbsp;&nbsp;&nbsp;5&nbsp;&nbsp;Tak&nbsp;&nbsp;Tak&nbsp;&nbsp;7<br>");
 document.write("ostatnia modyfikacja strony".fontcolor("red").bold().strike().fontsize(7)+"<br>"); 
 document.write(document.lastModified.fontcolor("maroon")); 
